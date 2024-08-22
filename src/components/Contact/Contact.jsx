32790f2df11bb02ba6f9c87f6ea142f6ec766cff
Olena Piatkivska <157681009+olenapiatkivska@users.noreply.github.com>
2024-08-22 19:14:16 +0300

    add redux

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import { deleteContact } from '../../redux/contactsSlice';
+import { useDispatch } from 'react-redux';
+import { FaUser } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa6';
+import css from './Contact.module.css';
+
+const Contact = ({ contact }) => {
+  const dispatch = useDispatch();
+  return (
+    <div className={css.contactBox}>
+      <div className={css.contactBoxWrapp}>
+        <p className={css.contactText}>
+          <FaUser />
+          {contact.name}
+        </p>
+        <p className={css.contactText}>
+          <FaPhone />
+          {contact.number}
+        </p>
+      </div>
+      <button
+        className={css.contactBtn}
+        onClick={() => dispatch(deleteContact(contact.id))}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+import { initialState } from './initialState';
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: initialState.contacts,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.items.push(action.payload);
+      },
+      prepare(name, number) {
+        return {
+          payload: {
+            id: nanoid(),
+            name,
+            number,
+          },
+        };
+      },
+    },
+  },
+  deleteContact: {
+    reducer(state, action) {
+      state.items = state.items.filter(item => {
+        item.id !== action.payload;
+      });
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const selectContacts = state => state.contacts.items;
+export const contactsReducer = contactsSlice.reducer;
